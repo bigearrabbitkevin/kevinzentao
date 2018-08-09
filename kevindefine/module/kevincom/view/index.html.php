5bf5ebf3f7de8d27c8d1b397697c00e9aef96893
Kevin Yang <39578745+bigearrabbitkevin@users.noreply.github.com>
2018-08-09 17:51:09 +0800

    主页显示样式修改

--- kevindefine/module/kevincom/view/index.html.php
+++ kevindefine/module/kevincom/view/index.html.php
@@ -1,182 +1,31 @@
 <?php
 include 'header.html.php';
-include '../../common/view/datepicker.html.php';
 ?>
-<div class='main' style="overflow:auto; ">
-	<div class="cards pd-0 mg-0">
-		<div class="card" style="border: 0">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin系列插件的安装说明 </h4>
-			</div>
-			<div class="card-content text-muted">1、安装正确的禅道版本，<br> 2、通过禅道的插件平台，安装插件。<br>3、如果有必要，进行手动的数据库升级。  </div>
-			<div class="card-actions">
-				<div style="margin-bottom: 10px">
-					<strong class="text-muted">作者: </strong>Kevin&lt;3301647@qq.com&gt; &nbsp;<strong class="text-muted">更新时间: </strong>2016-06-20 &nbsp; </div>
-				<div>
-					<div class="pull-right" style="margin-top: -25px">
-						<div class="btn-group">
-							<a href="http://www.zentao.net/extension.html" class="btn">禅道官方插件平台</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="card">
-			<div class="card pull-left">
-				<div class="card-heading">
-					<h4 class="mg-0">
-						<small class="label-num">77</small>
-						<a href="http://www.zentao.net/extension-viewExt-77.html">百度echarts</a>
-						<small class="label label-warning">第三方</small>          </h4>
-				</div>
-				<div class="card-content text-muted">
-					百度的Echarts表格插件3.0 (assemble to zentao by Kevin).    
-					<div id="bs-example-navbar-collapse-1" class="pull-right" >
-						<a href="http://echarts.baidu.com/index.html" class="btn">首页</a>
-						<a href="http://echarts.baidu.com/tutorial.html" class="btn">教程</a> 
-						<a href="http://echarts.baidu.com/api.html" class="btn">API</a>
-						<a href="http://echarts.baidu.com/examples.html" class="btn">实例</a>
-						<a href="http://echarts.baidu.com/echarts2/" class="btn">2.0</a> </div></div>
-				<div class="card-actions">
-					<div style="margin-bottom: 10px"><strong class="text-muted">免费</strong>&nbsp; </div>
-					<div class="pull-right" style="margin-top: -25px">
-						<div class="btn-group">
-							<a href="http://www.zentao.net/extension-viewExt-77.html" class="btn">详情</a>
-							<a href="http://www.zentao.net/extension-browseRelease-77-front.html" class="btn">下载</a>
-						</div>
-					</div>
-				</div>
-			</div>			
-			
-
-			<div class="card pull-right">
-				<div class="card-heading">
-					<h4 class="mg-0">
-						<small class="label-num">83</small>
-						<a href="http://www.zentao.net/extension-viewExt-83.html">Kendo UI</a>
-						<small class="label label-warning">第三方</small>          </h4>
-				</div>
-				<div class="card-content text-muted">
-					Kendo U，Apache v2开源授权        </div>
-				<div class="card-actions">
-					<div style="margin-bottom: 10px">
-
-						<div style="margin-bottom: 10px"><strong class="text-muted">免费</strong>&nbsp; </div>
-						<div class="pull-right" style="margin-top: -25px">
-							<div class="btn-group">
-								<a href="http://www.zentao.net/extension-viewExt-83.html" class="btn">详情</a>
-								<a href="http://www.zentao.net/extension-browseRelease-83-front.html" class="btn">下载</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="card">
-		<div class="card pull-left">
-			<div class="card-heading">
-				<h4 class="mg-0">
-					<small class="label-num">79</small>
-					Kevin图表 百度Echarts示例      </h4>
-			</div>
-			<div class="card-content text-muted">
-				Kevin图表, 百度的Echarts表格插件 (assemble to zentao by Kevin). 插件示例
-				强大的表格插件来了~~   <br>  第一次安装时，请手动安装数据: zentao\db\kevinchart.install.sql  </div>
-			<div class="card-actions">
-				<div style="margin-bottom: 10px"><strong class="text-muted">免费</strong>&nbsp; </div>
-
-			</div>
-		</div>			
-			
-		<div class="card pull-left">
-			<div class="card-heading">
-				<h4 class="mg-0">
-					<small class="label-num">84</small>
-					Kevin Kendo UI 示例       </h4>
-			</div>
-			<div class="card-content text-muted">
-				Kevin Kendo UI Sample.     <br>  第一次安装时，请手动安装数据: zentao\db\kevinkendoui.install.sql    </div>
-		</div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin日历</h4>
-			</div>
-			<div class="card-content text-muted">
-				可以自定义日历，可以用于项目工时统计        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin定义</h4>
-			</div>
-			<div class="card-content text-muted">
-				方便产品经理查看多个项目的任务，
-				项目下的需求，bug的筛选，像产品下的一样。产品需求分解，我的需求板块        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin默认值 </h4>
-			</div>
-			<div class="card-content text-muted">
-				创建需求时设置优先级默认值，创建任务时设置和需求相关的默认值        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin 软件 </h4>
-			</div>
-			<div class="card-content text-muted">
-				Kevin 软件 管理不同软件，不同版本，上传下载文件。组软件管理        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin工时</h4>
-			</div>
-			<div class="card-content text-muted">
-				基于todo做的工时记录，项目工时统计，考勤管理.        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin登录 ldap AD认证 权限  </h4>
-			</div>
-			<div class="card-content text-muted">
-				ldap ad 域认证登录、域用户映射表、登录的默认密码控制、权限控制设定的优化界面。        </div>
-			<div class="card-actions">
-				<div style="margin-bottom: 10px"><strong class="text-muted">免费</strong>&nbsp; </div>
-				<div class="pull-right" style="margin-top: -25px">
-					<div class="btn-group">
-						<a href="http://www.zentao.net/extension-viewExt-63.html" class="btn">详情</a>
-						<a href="http://www.zentao.net/extension-browseRelease-63-front.html" class="btn">下载</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">Kevin 设备管理</h4>
-			</div>
-			<div class="card-content text-muted">
-				Kevin Device        </div>
-		</div>
-		<div class="card">
-			<div class="card-heading">
-				<h4 class="mg-0">
-					<small class="label-num">68</small>
-					<a href="http://www.zentao.net/extension-viewExt-68.html">Kevin 解决snoopy socket被禁用补丁</a>
-					<small class="label label-warning">第三方</small>          </h4>
-			</div>
-			<div class="card-content text-muted">
-				解决snoopy socket被禁用补丁。浪云点等php服务器，基本上禁用了socket功能，导致后台=》插件=》获得插件：失败。        </div>
-			<div class="card-actions">
-				<div style="margin-bottom: 10px"><strong class="text-muted">免费</strong>&nbsp; </div>
-				<div class="pull-right" style="margin-top: -25px">
-					<div class="btn-group">
-						<a href="http://www.zentao.net/extension-viewExt-68.html" class="btn">详情</a>
-						<a href="http://www.zentao.net/extension-browseRelease-68-front.html" class="btn">下载</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<?php 
+//定义首页的列表
+$MainItemList[]= array('module'=>'kevinhours', 'method'=> 'index', 'name'=> '工时考勤','desc'=>'工时考勤,加班单，工时单');
+$MainItemList[]= array('module'=>'kevinsvn', 'method'=> 'index', 'name'=> 'SVN','desc'=>'SVN管理');
+$MainItemList[]= array('module'=>'kevinuser', 'method'=> 'classlist', 'name'=> '用户','desc'=>'用户管理');
+$MainItemList[]= array('module'=>'kevinclass', 'method'=> 'index', 'name'=> '分类','desc'=>'数据分类');
+$MainItemList[]= array('module'=>'kevincalendar', 'method'=> 'todo', 'name'=> '日历','desc'=>'日历、日志管理');
+$MainItemList[]= array('module'=>'kevindevice', 'method'=> 'devlist', 'name'=> '设备','desc'=>'设备管理');
+$MainItemList[]= array('module'=>'kevinstore', 'method'=> 'itemlist', 'name'=> '仓库','desc'=>'仓库管理');
+$MainItemList[]= array('module'=>'kevinsoft', 'method'=> 'softlist', 'name'=> '软件','desc'=>'软件管理');
+$MainItemList[]= array('module'=>'kevinkendoui', 'method'=> 'sample', 'name'=> 'Kendo','desc'=>'Kendo示例');
+$MainItemList[]= array('module'=>'kevinchart', 'method'=> 'index', 'name'=> 'Echarts','desc'=>'百度Echarts表格示例');
+?>
+<div class='main' >
+    <div class="cards">
+      <?php foreach($MainItemList as $appItem){
+          if(!common::hasPriv($appItem['module'],$appItem['method']))continue;//没有权限跳过
+          ?>
+          <div class="col-md-4 col-sm-6 col-lg-3">
+            <a href="<?php echo helper::createLink($appItem['module'],$appItem['method']);?>" class="card" title="<?php echo $appItem['name'];?>">
+              <div class="card-heading"><strong><?php echo $appItem['name'];?></strong></div>
+              <div class="card-content text-muted"><?php echo $appItem['desc'];?></div>
+            </a>
+          </div>
+      <?php }?>
+    </div>
 </div>
 <?php include '../../kevincom/view/footer.html.php'; ?>

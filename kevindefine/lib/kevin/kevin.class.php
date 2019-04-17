3f2dc1fa5b03d913f18878f8c8ffff02a75d9128
Kevin Yang <39578745+bigearrabbitkevin@users.noreply.github.com>
2019-04-17 13:55:16 +0800

    cal_days_in_month 添加 ，适配linux里面没有的情况

--- kevindefine/lib/kevin/kevin.class.php
+++ kevindefine/lib/kevin/kevin.class.php
@@ -6,22 +6,29 @@
  * http://kevincom.sourceforge.net/
  * The author disclaims copyright to this source code.  In place of
  * a legal notice, here is a blessing:
- * 
+ *
  *  May you do good and not evil.
  *  May you find forgiveness for yourself and forgive others.
  *  May you share freely, never taking more than you give.
  */
 
+if (!function_exists('cal_days_in_month')) {
+	define('CAL_GREGORIAN', 0);
+	function cal_days_in_month($calendar, $month, $year) {
+		return date('t', mktime(0, 0, 0, $month, 1, $year));
+	}
+}
+
 /**
  * The kevin class.
- * 
+ *
  * @package   framework
  */
 class kevin {
 
 	/**
 	 * echo message Die
-	 *  @param array $Errors
+	 * @param array $Errors
 	 *  like $Err[][], 2D
 	 */
 	public static function dieError($Errors) {
@@ -29,9 +36,9 @@ class kevin {
 			foreach ($Errors as $msgArr) {
 				if (is_array($msgArr)) {
 					foreach ($msgArr as $msg) {
-						echo $msg . "<br>";
+						echo $msg."<br>";
 					}
-				} else echo $msg . "<br>";
+				} else echo $msg."<br>";
 			}
 		} else echo $Errors;
 		die();
@@ -47,8 +54,8 @@ class kevin {
 	}
 
 	/**
-	 * if has message,echo and Die 
-	 *  @param array $Errors
+	 * if has message,echo and Die
+	 * @param array $Errors
 	 *  like $Err[][], 2D
 	 */
 	public static function dieIfError(&$Errors) {
@@ -59,16 +66,16 @@ class kevin {
 	/**
 	 * Format time 0915 to 09:15
 	 *  20150810 to 2015-08-10
-	 * @param  string $time 
+	 * @param  string $time
 	 * @access public
 	 * @return string
 	 */
 	public static function formatTime($time) {
 		switch (strlen($time)) {
 			case 4:
-				return substr($time, 0, 2) . ':' . substr($time, 2, 2);
+				return substr($time, 0, 2).':'.substr($time, 2, 2);
 			case 8:
-				return substr($time, 0, 4) . '-' . substr($time, 4, 2) . '-' . substr($time, 6, 2);
+				return substr($time, 0, 4).'-'.substr($time, 4, 2).'-'.substr($time, 6, 2);
 			default:
 				return $time;
 		}
@@ -76,10 +83,10 @@ class kevin {
 
 	/**
 	 * The getBeginEndTime.
-	 * 
+	 *
 	 * @access public
-	 * @param  类型 $type 
-	 * @param  int $delta 
+	 * @param  类型  $type
+	 * @param  int $delta
 	 * @return array{begin,end}.
 	 */
 	public static function getBeginEndTime($period) {
@@ -87,28 +94,26 @@ class kevin {
 			$type = 'thisMonth';
 		}
 		//$this->app->loadClass('date');
-		$period		 = strtolower($period);
+		$period      = strtolower($period);
 		$inputLength = strlen($period);
-		$isNumeric	 = is_numeric($period);
+		$isNumeric   = is_numeric($period);
 		//此处组合为年+月 共6位数字
 		if ($isNumeric && 6 == $inputLength) {
-			$period .= '0';
+			$period      .= '0';
 			$inputLength = 7;
 		}
 		//此处判断为最近时间,小于等于4位数字
 		if ($isNumeric && $inputLength < 4) {
-			$begin	 = date('Y-m-d', strtotime('-' . $period . ' day'));
-			$end	 = date('Y-m-d', strtotime('+1 day'));
+			$begin = date('Y-m-d', strtotime('-'.$period.' day'));
+			$end   = date('Y-m-d', strtotime('+1 day'));
 		} else if ($isNumeric && 4 == $inputLength) {
-			$begin	 = $period . '-01-01 00:00:00';
-			$end	 = $period . '-12-31 23:59:59';
-			;
-		}
-		//此处判断的7为数字组成为年+月+季度
+			$begin = $period.'-01-01 00:00:00';
+			$end   = $period.'-12-31 23:59:59';;
+		} //此处判断的7为数字组成为年+月+季度
 		else if ($isNumeric && 7 == $inputLength) {
 			//日期截取
-			$year	 = substr($period, 0, 4);
-			$month	 = substr($period, 4, 2);
+			$year  = substr($period, 0, 4);
+			$month = substr($period, 4, 2);
 			if ('00' == $month) {
 				$month = '';
 			}
@@ -118,130 +123,130 @@ class kevin {
 			}
 			$monthArray = kevin::getMonthBySeason($month, $season);
 			if (1 == count($monthArray)) {
-				$currentMonth	 = current($monthArray);
-				$begin			 = $year . '-' . $currentMonth . '-01';
+				$currentMonth = current($monthArray);
+				$begin        = $year.'-'.$currentMonth.'-01';
 				return kevin::getMonthBeginEnd($begin);
 			} else {
-				$begin			 = $year . '-' . current($monthArray) . '-01 00:00:00';
-				$last			 = $year . '-' . end($monthArray) . '-' . '01 00:00:00';
-				$Array			 = kevin::getMonthBeginEnd($last);
-				$Array['begin']	 = $begin;
+				$begin          = $year.'-'.current($monthArray).'-01 00:00:00';
+				$last           = $year.'-'.end($monthArray).'-'.'01 00:00:00';
+				$Array          = kevin::getMonthBeginEnd($last);
+				$Array['begin'] = $begin;
 				return $Array;
 			}
 		} else {
 			switch ($period) {
 				case 'today':
-					$begin	 = kevin::formatTime(date::today());
-					$end	 = $begin;
+					$begin = kevin::formatTime(date::today());
+					$end   = $begin;
 					break;
 				case 'yesterday':
-					$begin	 = kevin::formatTime(date::yesterday());
-					$end	 = $begin;
+					$begin = kevin::formatTime(date::yesterday());
+					$end   = $begin;
 					break;
 				case 'thisweek':
-					return(date::getThisWeek());
+					return (date::getThisWeek());
 					break;
 				case 'lastweek':
-					return(date::getLastWeek());
+					return (date::getLastWeek());
 					break;
 				case 'nextmonth':
-					return(kevin::getNextMonth());
+					return (kevin::getNextMonth());
 					break;
 				case 'thismonth':
-					return(date::getThisMonth());
+					return (date::getThisMonth());
 					break;
 				case 'lastmonth':
-					return(date::getLastMonth());
+					return (date::getLastMonth());
 					break;
 
 				case 'thisseason':
-					return(date::getThisSeason());
+					return (date::getThisSeason());
 					break;
 				case 'thisyear':
-					return(date::getThisYear());
+					return (date::getThisYear());
 					break;
 				case 'lastyear':
-					return(date::getLastYear());
+					return (date::getLastYear());
 					break;
 				case 'future':
-					$begin	 = kevin::formatTime(date::tomorrow());
-					$end	 = '2109-01-01'; //end furture
+					$begin = kevin::formatTime(date::tomorrow());
+					$end   = '2109-01-01'; //end furture
 					break;
 				case 'all':
-					$begin	 = '1970-01-01';
-					$end	 = '2109-01-01'; //end furture
+					$begin = '1970-01-01';
+					$end   = '2109-01-01'; //end furture
 					break;
 				case 'before':
-					$begin	 = '1970-01-01';
-					$end	 = kevin::formatTime(date::yesterday());
+					$begin = '1970-01-01';
+					$end   = kevin::formatTime(date::yesterday());
 					break;
 				default:
-					$begin	 = kevin::formatTime($period);
-					$end	 = $begin;
+					$begin = kevin::formatTime($period);
+					$end   = $begin;
 			}
-			if (10 == strlen($end)) $end = $end . ' 23:59:59';
+			if (10 == strlen($end)) $end = $end.' 23:59:59';
 		}
 		return array('begin' => $begin, 'end' => $end);
 	}
 
 	/**
 	 * The get year month
-	 * 
+	 *
 	 * @access public
-	 * @param  string $period 
+	 * @param  string $period
 	 * @return array of year month.
 	 */
 	public static function getFirstYearMonth($period) {
-		$arrayK	 = kevin::getBeginEndTime($period);
-		$begin	 = $arrayK['begin'];
-		$year	 = substr($begin, 0, 4);
-		$month	 = substr($begin, 5, 2);
+		$arrayK = kevin::getBeginEndTime($period);
+		$begin  = $arrayK['begin'];
+		$year   = substr($begin, 0, 4);
+		$month  = substr($begin, 5, 2);
 		return array('year' => $year, 'month' => $month);
 	}
 
 	/**
 	 * The getMonth.
-	 * 
+	 *
 	 * @access public
-	 * @param  date $now 
-	 * @param  int $delta 
+	 * @param  date $now
+	 * @param  int  $delta
 	 * @return int of month[1 to 12].
 	 */
 	public static function getMonth($delta, $now = 'time()') {
-		$delta = (int) $delta;
+		$delta = (int)$delta;
 		if ($now == 'time()') {
 			$now = time();
 		}
-		$m	 = (int) date('m', $now);
-		$m	 = $m + $delta;
-		$m	 = $m % 12;
+		$m = (int)date('m', $now);
+		$m = $m + $delta;
+		$m = $m % 12;
 		if ($m <= 0) {
-			$m+=12;
+			$m += 12;
 		}
 		return $m;
 	}
 
 	/**
 	 * The strtotime.
-	 * 
+	 *
 	 * @access public
-	 * @param  string $time 
+	 * @param  string $time
 	 * @return new date.
 	 */
 	public static function getMonthBeginEnd($time) {
-		$newTime	 = strtotime($time);
-		$begin		 = date('Y-m-01 00:00:00', $newTime); //to first day
-		$tempTime	 = strtotime($begin);
-		$end		 = date('Y-m-d', strtotime("+1 month -1 day", $tempTime)) . ' 23:59:59'; //to last day
+		$newTime  = strtotime($time);
+		$begin    = date('Y-m-01 00:00:00', $newTime); //to first day
+		$tempTime = strtotime($begin);
+		$end      = date('Y-m-d', strtotime("+1 month -1 day", $tempTime)).' 23:59:59'; //to last day
 		return array('begin' => $begin, 'end' => $end);
 	}
 
 	/**
 	 * The get month by Season.
-	 * 
+	 *
 	 * @access public
-	 * @param  int $month 
-	 * @param  int $season 
+	 * @param  int $month
+	 * @param  int $season
 	 * @return array of month.
 	 */
 	public static function getMonthBySeason($month = '', $season = '') {
@@ -254,39 +259,39 @@ class kevin {
 			return $monthArray;
 		}
 		for ($i = 1; $i < 4; $i++) {
-			$currentMonth	 = sprintf("%02d", 3 * ($season - 1) + $i);
-			$monthArray[]	 = $currentMonth;
+			$currentMonth = sprintf("%02d", 3 * ($season - 1) + $i);
+			$monthArray[] = $currentMonth;
 		}
 		return $monthArray;
 	}
 
 	/**
 	 * Get begin and end time of this month.
-	 * 
+	 *
 	 * @access public
 	 * @return array
 	 */
 	public static function getNextMonth() {
-		$begin	 = date('Y-m', strtotime('next month')) . '-01 00:00:00';
-		$year	 = substr($begin, 0, 4);
-		$month	 = substr($begin, 5, 2);
-		$days	 = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-		$end	 = $year . '-' . $month . '-' . $days . ' 23:59:59';
+		$begin = date('Y-m', strtotime('next month')).'-01 00:00:00';
+		$year  = substr($begin, 0, 4);
+		$month = substr($begin, 5, 2);
+		$days  = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+		$end   = $year.'-'.$month.'-'.$days.' 23:59:59';
 		return array('begin' => $begin, 'end' => $end);
 	}
 
 	/**
 	 * The get month list.
-	 * 
+	 *
 	 * @access public
-	 * @param  int $month 
-	 * @param  int $season 
+	 * @param  int $month
+	 * @param  int $season
 	 * @return array of month.
 	 */
 	public static function getMonthList($begin, $end, & $monthArray) {
-		$monthArray	 = array();
-		$month		 = date('Y-m', strtotime($begin)); //to last day
-		$monthEnd	 = date('Y-m', strtotime($end)); //to last day
+		$monthArray = array();
+		$month      = date('Y-m', strtotime($begin)); //to last day
+		$monthEnd   = date('Y-m', strtotime($end)); //to last day
 		if ($monthEnd < $month) return; //error
 
 		$monthArray[$month] = $month;
@@ -302,8 +307,8 @@ class kevin {
 	/* show hours like 3.5 for 210 minutes */
 
 	public static function showHours($iMinutes, $i = 1) {
-		$workhour	 = ((float) $iMinutes / 60);
-		$workhour	 = number_format($workhour, $i);
+		$workhour = ((float)$iMinutes / 60);
+		$workhour = number_format($workhour, $i);
 		return $workhour;
 	}
 
@@ -311,33 +316,32 @@ class kevin {
 
 	public static function showWorkHours($iMinutes) {
 		if ($iMinutes > 60) {
-			if (0 == (int) ($iMinutes % 60)) return ((int) ($iMinutes / 60)) . ':00';
-			return ((int) ($iMinutes / 60)) . ':' . ((int) ($iMinutes % 60));
+			if (0 == (int)($iMinutes % 60)) return ((int)($iMinutes / 60)).':00';
+			return ((int)($iMinutes / 60)).':'.((int)($iMinutes % 60));
+		} else if ($iMinutes == 60) {
+			return ((int)$iMinutes / 60).':00';
 		}
-		else if ($iMinutes == 60) {
-			return((int) $iMinutes / 60) . ':00';
-		}
-		return '0:' . ((int) $iMinutes % 60);
+		return '0:'.((int)$iMinutes % 60);
 	}
 
 	/**
 	 * The strtotime.
-	 * 
+	 *
 	 * @access public
-	 * @param  string $time 
-	 * @param  date $now 
+	 * @param  string $time
+	 * @param  date   $now
 	 * @return new date.
 	 */
 	public static function strtotimeMonth($time, $now = 'time()') {
 		if ($now == 'time()') {
 			$now = time();
 		}
-		$newTime		 = strtotime($time, $now);
-		$firstday		 = date('Y-m-01', $now); //to first day
-		$newFirstday	 = strtotime("$firstday " . $time);
-		$newFirstday_s	 = date('Y-m-d', $newFirstday);
-		$newLastday_s	 = date('Y-m-d', strtotime("$newFirstday_s +1 month -1 day")); //to last day
-		$newLastday		 = strtotime($newLastday_s);
+		$newTime       = strtotime($time, $now);
+		$firstday      = date('Y-m-01', $now); //to first day
+		$newFirstday   = strtotime("$firstday ".$time);
+		$newFirstday_s = date('Y-m-d', $newFirstday);
+		$newLastday_s  = date('Y-m-d', strtotime("$newFirstday_s +1 month -1 day")); //to last day
+		$newLastday    = strtotime($newLastday_s);
 
 		if ($newTime > $newLastday) {
 			return $newLastday;
@@ -346,5 +350,6 @@ class kevin {
 		}
 		return $newTime;
 	}
+
 
 }

1bb9b1bb53ef65aae13eed5a8c3f4b3cc8c46951
Kevin Yang <39578745+bigearrabbitkevin@users.noreply.github.com>
2019-06-02 09:23:27 +0800

    kevin_ERROR

--- kevindefine/www/js/kevin/kevin.js
+++ kevindefine/www/js/kevin/kevin.js
@@ -1,3 +1,11 @@
+/*!
+ * keivn js class
+ * GitHub: https://github.com/bigearrabbitkevin/kevinzentao/trunk/kevindefine/www/js/kevin
+ * version 1.0.0.1
+ * Copyright kevin; 
+ * Licensed MIT
+ */
+
 function GetNewPeriodMonth(type,year,month)
 {
 	var currentDate;
@@ -27,4 +35,122 @@ function GetNewPeriodMonth(type,year,month)
 		currentDate = 	year + month;
 	}
 	return currentDate;
+}
+
+//kendogrid 公共变量
+var kevin_kendogrid_div; //grid 变量表示$("#grid")
+var kevin_kendogrid; //grid 变量表示$("#grid").kendoGrid
+var kevin_kendogrid_columnsSets = '';//表格数据
+var kevin_kendogrid_DataArray = []; //global data array
+var kevin_kendogrid_fileName = '';//输出文件名的 公共变量
+var kevin_kendogrid_dataItem = '';//表格行内数据
+var kevin_kendogrid_toolbar = '';//toolbar
+
+//var kevin_today 在view的页面里面定义,要改为在这里定义，杨海华
+var kevin_today_obj = new Date();                           // 创建 Date 对象。
+var kevin_today = '';//today string
+
+var kevin_ModelWnd; //模态窗口
+var kevin_detailsTemplate;//详细模版
+
+/**
+ * 初始化参数
+ * 2019-06-02
+ */
+function kevin_base_initialParam() {
+	kevin_kendogrid_div = $("#grid"); //referrer to grid
+	kevin_today_obj = new Date(); 
+	kevin_today = kevin_today_obj.getYear()+ (kevin_today_obj.getMonth() + 1) + kevin_today_obj.getDate(); // 获取日。
+	kevin_kendogrid_fileName = 'Kendo_UI_Grid_Export_' + kevin_today + '.xlsx';
+}
+
+/**
+ * 定义kevin_ERROR的类变量
+ * 2019-05-07
+ */
+var kevin_ERROR = {
+	createNew: function () {
+		var err = {};
+		err.errcode = 0;
+		err.errmsg = '';
+		err.data = [];
+		err.isError = function () {
+			return 0 != err.errcode;
+		};
+		return err;
+	}
+};
+
+/**
+ * Check input object is kevin_ERROR or not
+ * 2019-05-07
+ * @param kevin_ERROR iData
+ * @return kevin_ERROR
+ */
+function kevin_CheckJsonReturnData(iData) {
+	var ret = kevin_ERROR.createNew();
+	ret.errcode = 1;
+
+	if (iData === null || !(iData.constructor === Object)) {
+		ret.errmsg = 'It is not an Object : ' + iData;
+		return ret;
+	}
+
+	if (!(iData.hasOwnProperty("errcode"))) {
+		ret.errmsg = 'Error! No errcode property in json!';
+		return ret;
+	}
+	else if (iData.errcode > 0) {
+		ret.errmsg = (iData.hasOwnProperty("errmsg")) ? iData.errmsg : 'Unkown Error!';
+		return ret;
+	}
+	if (!(iData.hasOwnProperty("data"))) {
+		ret.errmsg = 'Error! No Data property in json!';
+		return ret;
+	}
+	//获得code
+	ret.errcode = iData.errcode;
+	if (iData.errcode > 0) {
+		ret.errmsg = ((iData.hasOwnProperty("errmsg")) ? iData.errmsg : 'Unkown Error!');
+	}
+	return ret; //no error
+}
+
+ /**
+ * 展示kendogrid数据
+ * kevin_kendogrid_DataArray = null表示全部显示列
+ * 设定kevin_kendogrid的引用
+ * 2019-05-07
+ */
+function kevin_kendogrid_updateGridData() {
+	var record = 0;
+	$("#grid").data("kendoGrid").destroy();
+	$("#grid").html("");
+	$("#grid").kendoGrid({
+		columnMenu: true,
+		columns: kevin_kendogrid_columnsSets,
+		dataSource: kevin_kendogrid_DataArray,
+		dataBinding: function () {
+			kevin_kendogrid = this;
+			record = (this.dataSource.page() - 1) * this.dataSource.pageSize();
+		},
+		selectable: "row",
+		groupable: true,
+		sortable: true,
+		pageable: {
+			pageSize: 15,
+			pageSizes: [10, 15, 20, 50, 100, 200, 500, 1000, 2000],
+			messages: {
+				display: "{0}-{1} 共{2}个",
+				empty: "无数据",
+				itemsPerPage: "项每页"
+			}
+		},
+		resizable: true,
+		toolbar: kevin_kendogrid_toolbar,
+		excel: {
+			fileName: kevin_kendogrid_fileName,
+			allPages: true//是否导出所有页中的数据
+		}
+	});
 }
